ccc0c75caf2533ac8526e64dcdb644e7ba3f5086
nsiscar <nsiscar01@gmail.com>
2024-10-14 19:15:31 +0200

    javascrpt

---
+++ Java/js.js
@@ -0,0 +1,26 @@
+// Buscador de Proyectos
+document.getElementById('btn-buscar').addEventListener('click', function() {
+    const searchQuery = document.getElementById('buscar-proyectos').value.toLowerCase();
+    const projects = document.querySelectorAll('.card');
+    
+    projects.forEach((project) => {
+        const title = project.querySelector('.card-title').textContent.toLowerCase();
+        if (title.includes(searchQuery)) {
+            project.style.display = '';
+        } else {
+            project.style.display = 'none';
+        }
+    });
+});
+
+// Validación y modal de confirmación
+document.getElementById('form-contacto').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var email = document.getElementById('email').value;
+    if (email.includes('@')) {
+        var modal = new bootstrap.Modal(document.getElementById('modalConfirmacion'));
+        modal.show();
+    } else {
+        alert('Por favor, introduce un correo electrónico válido.');
+    }
+});
